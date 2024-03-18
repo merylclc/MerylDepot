178eee674efcf71f61536e6cb8b6a8fb48e51187
Farès <elitegame4200@gmail.com>
2024-03-18 14:53:16 +0100

    Update TD_GitHub.h

--- TD_GitHub.h
+++ TD_GitHub.h
@@ -15,12 +15,3 @@ int sommeNombre(int nb);
 float salaireNet(int salaire);
 
 int plusPetit(int a, int b, int c);
-
-//Fonctions à réaliser par l'étudiant 3
-bool estImpair(int nb);
-
-float moyenne(int nb1, int nb2, int nb3);
-
-float reduction(int prix, int reduc);
-
-int nombreMedian(int a, int b, int c);
