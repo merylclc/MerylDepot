759dbc783c62bf5f2e80d5d2de17e4eabaf2346b
merylclc <100871503+merylclc@users.noreply.github.com>
2024-03-18 14:32:17 +0100

    Add files via upload

---
+++ TD_GitHub.cpp
@@ -0,0 +1,23 @@
+//Fonctions réalisées par l'étudiant 1 :
+
+
+
+
+
+
+
+
+
+
+//Fonctions réalisées par l'étudiant 2 :
+
+
+
+
+
+
+
+
+
+
+//Fonctions réalisées par l'étudiant 3 :
---
+++ TD_GitHub.h
@@ -0,0 +1,26 @@
+//Fonctions à réaliser par l'étudiant 1
+bool estMajeur(int age);
+
+int exposant(int nbre, int exp);
+
+float TVA(int prix);
+
+int plusGrand(int a, int b, int c);
+
+//Fonctions à réaliser par l'étudiant 2
+bool estPair(int nb);
+
+int sommeNombre(int nb);
+
+float salaireNet(int salaire);
+
+int plusPetit(int a, int b, int c);
+
+//Fonctions à réaliser par l'étudiant 3
+bool estImpair(int nb);
+
+float moyenne(int nb1, int nb2, int nb3);
+
+float reduction(int prix, int reduc);
+
+int nombreMedian(int a, int b, int c);
